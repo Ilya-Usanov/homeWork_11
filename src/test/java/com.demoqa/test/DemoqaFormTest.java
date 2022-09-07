eda70209eb672d416dd438ff412a849d1c8f23ea
Ilya.Usanov <Ilya.Usanov@rt.ru>
2022-09-07 14:35:23 +0300

    update tests with jenkins

--- src/test/java/com.demoqa/test/DemoqaFormTest.java
+++ src/test/java/com.demoqa/test/DemoqaFormTest.java
@@ -4,9 +4,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.data.TestData;
 import com.demoqa.pages.RegistrationFormPage;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 
@@ -21,9 +26,23 @@ public class DemoqaFormTest {
     @BeforeAll
     static void configure() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("last sreenshot");
+        Attach.pageSource();
+        Attach.browseConsoleLogs();
     }
 
     @Test
